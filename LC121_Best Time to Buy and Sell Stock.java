674225942ab0491cadad664370562052c0d70a47
thananauto <thananjayan1988@gmail.com>
2026-08-15 13:34:41 +0530

    LC 121

---
+++ LC121_Best Time to Buy and Sell Stock.java
@@ -0,0 +1,11 @@
+public class LC121_Best Time to Buy and Sell Stock {
+    public static int maxprofit(int[] prices){
+        int mini = prices[0];
+        int maxprofit = 0;
+        for(int i=1;i<prices.length;i++){
+            maxprofit = Math.max(maxprofit,prices[i]-mini);
+            mini = Math.min(mini,prices[i]);
+        }
+        return maxprofit;
+    }
+}
