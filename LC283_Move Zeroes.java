23391b79dfc4059a4b2a7c1e4c8b9b8bf5c3780e
thananauto <thananjayan1988@gmail.com>
2026-08-16 12:18:29 +0530

    LC 283 - i have added the pos tracker of the zeros ... while traversing if the pointer faces any non zero element the pos element where the zero is present would be swapped ... prev i did it using two pointer whose time complexity is O(n2) now it is O(n)

--- LC283_Move Zeroes.java
+++ LC283_Move Zeroes.java
@@ -1,16 +1,17 @@
 class LC283_MoveZeroes {
     public void moveZeroes(int[] nums) {
-        for(int i=0;i<nums.length-1;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i] == 0 && nums[j]!=0){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    break;
-                }
+        int pos =0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                int temp = nums[pos];
+                nums[pos] = nums[i];
+                nums[i] = temp;
+                pos++;
             }
         }
         
     }
-    
 }
+/*
+we are tracking the position of the next non-zero element
+*/
