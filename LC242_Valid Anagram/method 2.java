9e35c53488650f28147461c9dfb38a3f119b16c1
thananauto <thananjayan1988@gmail.com>
2026-08-16 23:15:57 +0530

    LC 242 we are sorthing the string using freq array (METHOD 2)

--- LC242_Valid Anagram/method 2.java
+++ LC242_Valid Anagram/method 2.java
@@ -1,15 +1,16 @@
+// using frequency array to count the no of char in the 1st and the 2nd string and then check the count both strings
 class Method2 {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
 
         int[] count = new int[26];
         for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            count[t.charAt(i) - 'a']--;
+            count[s.charAt(i) - 'a']++; // counts the number of characters in the first string
+            count[t.charAt(i) - 'a']--; // decrements the count for characters in the second string if found in next string
         }
 
         for (int i = 0; i < 26; i++) {
-            if (count[i] != 0) return false;
+            if (count[i] != 0) return false; // if any char are not in another string then return false
         }
 
         return true;
